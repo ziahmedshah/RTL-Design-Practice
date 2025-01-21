7d13f57ae45911deade3282c0aada5f6bf4dba46
ZiAhmedShah <155481761+ziahmedshah@users.noreply.github.com>
2025-01-21 12:17:49 +0500

    pos_n_neg_edge_det.sv

---
+++ pos_n_neg_edge_det.sv
@@ -0,0 +1,14 @@
+
+module pos_n_neg_edge_det ( input a, b  ,         
+                      input clk,           
+                      output out);           
+    reg   a_delay, b_delay;                          
+  
+  always @ (posedge clk) begin
+    a_delay <= a;
+    b_delay <= b;
+  end
+
+  assign out = (a & ~a_delay) | (~b & b_delay);            
+endmodule 
+ 
