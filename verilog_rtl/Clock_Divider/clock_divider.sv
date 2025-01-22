8c208d9b189fb9806930c3b5b8089c7c4cceb92a
ZiAhmedShah <155481761+ziahmedshah@users.noreply.github.com>
2025-01-22 11:11:14 +0500

    clock_divider.sv

--- verilog_rtl/Clock_Divider/clock_divider.sv
+++ verilog_rtl/Clock_Divider/clock_divider.sv
@@ -2,7 +2,7 @@ module clock_divider #(parameter WIDTH = 8) (
     input logic clk,           
     input logic rst,           
     input logic [WIDTH-1:0] div, 
-    output logic pwm_out      
+    output logic clk_out      
 );
 
     logic [WIDTH-1:0] counter; 
@@ -13,16 +13,16 @@ module clock_divider #(parameter WIDTH = 8) (
             counter <= 0; 
         else if (counter == div - 1)
             counter <= 0; 
-        else\par
+        else
             counter <= counter + 1; 
     end
 
     // PWM generation logic
     always_ff @(posedge clk or posedge rst) begin
         if (rst)
-            pwm_out <= 0;
+            clk_out <= 0;
         else
-            pwm_out <= (counter < (div >> 1)); // Toggle PWM at div/2
+            clk_out <= (counter < (div >> 1)); // Toggle PWM at div/2
     end
 
 endmodule
