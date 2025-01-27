84328f8ecbf3cab0c314ae1aa06aa50d139049e7
ZiAhmedShah <155481761+ziahmedshah@users.noreply.github.com>
2025-01-27 10:18:05 +0500

    Create tb.sv

---
+++ verilog_rtl/Pos&Neg_Edge_Detector/tb.sv
@@ -0,0 +1,31 @@
+module tb;
+	reg a, b;                                  
+	reg clk;                       
+  pos_n_neg_edge_det dut (  .a(a),.b(b),           
+    					 .clk(clk),
+                     .out(out));
+
+	// Generate a clock of 100MHz
+	always #5 clk = ~clk;           
+	
+
+	initial begin
+		clk <= 0;
+		a <= 0;
+      b <= 0;
+		#15 a <= 1;
+		#20 a <= 0;
+		#15 a <= 1;
+		#10 a <= 0;
+      		#15 b <= 1;
+		#20 b <= 0;
+		#15 b <= 1;
+		#10 b <= 0;
+		#20 $finish;
+	end	
+  
+  	initial begin
+      $dumpvars;
+      $dumpfile("dump.vcd");
+    end
+endmodule
